4f41693457eeef7293b204330f0331c641645155
SebGarcia4 <55549339+SebGarcia4@users.noreply.github.com>
2019-11-07 10:21:16 -0700

    Add files via upload

---
+++ js-map-analyzer-master/js-map-analyzer-master/solutions.js
@@ -0,0 +1,173 @@
+// SOLUTIONS TO MAP ANALYZER LEVELS
+
+// Global Variables that store row and column of tile that mouse is on
+let row, column;
+
+// Solution Functions...
+function level1Solution() {
+	// USE BINARY IF STATEMENT
+	if (column <= 4) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+
+
+	
+}
+
+function level2Solution() {
+	// USE CHAINED IF STATEMENT
+	if (column <= 2) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else if (column >= 3 && column <= 7) { 
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+	
+}
+
+function level3Solution() {
+	// USE BINARY IF STATEMENT
+	if (column <= 2) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else if (column >= 3 && column <= 7) { 
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}
+}
+
+function level4Solution() {
+	// USE BINARY IF STATEMENT
+	if (row <= 2) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else { 
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}
+	
+}
+
+function level5Solution() {
+	// USE CHAINED IF STATEMENT
+	if (row <= 2) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else if (row <=5) { 
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else { 
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+}
+
+function level6Solution() {
+	// USE BINARY IF STATEMENT
+
+	if (row <= 1) {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}else if (row <=5) { 
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else { 
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+}
+
+function level7Solution() {
+	// USE BINARY IF STATEMENT
+
+	if (row <= 3 & column <= 7) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (column <= 6) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row >= 5) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (column >= 8) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else { 
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+}
+
+function level8Solution() {
+	// USE BINARY IF STATEMENT
+	if (row >= 6 && row <= 7 && column <= 1) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row <= 4 && column <= 1) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row >= 6 && row <= 7 && column <= 11 && column >= 3) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row >= 0 && row <= 4 &&  column <= 11 && column >= 3) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else { 
+		document.getElementById("currentTile").innerHTML = "tree"; // Set output text
+	}
+}
+
+function level9Solution() {
+	// USE BINARY IF STATEMENT
+	if (row <= 5 && column <= 5) {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}else if ( column <= 6 && row <= 6) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}
+	
+}
+
+function level10Solution() {
+	// USE CHAINED IF STATEMENT
+	if (row <= 7 && column <= 1) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else if (row <= 7 && column<= 3) {
+		document.getElementById("currentTile").innerHTML = "tree"; // Set output text
+	}else if (row <= 7 && column<= 7) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+	document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+}
+
+function level11Solution() {
+	// USE CHAINED IF STATEMENT
+	if (row <= 3 && column <= 5) {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}else if (row <= 7 && column<= 5) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row <= 3 && column<= 11) {
+		document.getElementById("currentTile").innerHTML = "hills"; // Set output text
+	}else {
+	document.getElementById("currentTile").innerHTML = "tree"; // Set output text
+	}
+}
+
+function level12Solution() {
+	// USE BINARY IF STATEMENT
+	if (row <= 4 && column <= 5) {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}else if (row <= 7 && column<= 5) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else if (row <= 4 && column<= 11) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+	document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}
+}
+
+function level13Solution() {
+	// USE BINARY IF STATEMENT
+	if (row >= 2 && row <= 5 && column >= 4 && column <= 9) {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}else {
+		document.getElementById("currentTile").innerHTML = "tree"; // Set output text
+	}
+}
+
+function level14Solution() {
+	// USE CHAINED IF STATEMENT
+	if (row >= 1 && row <= 5 && column >= 2 && column <= 4) {
+		document.getElementById("currentTile").innerHTML = "tree"; // Set output text
+	}else if (row >= 3 && row <= 5 && column >= 7 && column <= 10) {
+		document.getElementById("currentTile").innerHTML = "water"; // Set output text
+	}else {
+		document.getElementById("currentTile").innerHTML = "sand"; // Set output text
+	}
+}
